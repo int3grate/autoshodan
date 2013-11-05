e28329ad12cf0ed31c5d3945f32b57d9ee89234f
Your Name <you@example.com>
2013-11-04 22:10:31 -0600

    Updated config variables

--- config.py
+++ config.py
@@ -1,1 +1,11 @@
+# shodan API key
 SHODAN_API_KEY = "ySkEl7SkCyKdoSOmhl1jO5hS0on2thhy"
+
+# query other ports automatically to obtain data?
+AUTO_LOAD_OTHER_POTOCOLS = True
+
+# save query cache to file?
+SAVE_QUERY_CACHE = True
+
+# query cache file
+CACHE_FILE = 'cache.dat'
